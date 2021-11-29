19beda6ce76373d02952f78f08b9020f8ed69c79
Matthijs <47416015+Matthijsvanspelde@users.noreply.github.com>
2021-11-29 13:57:54 +0100

    Add Player Controller

---
+++ Bierens Debt Collector/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+    [Header("Y Clamp")]
+    [SerializeField] private float minYClamp = -90f;
+    [SerializeField] private float maxYClamp = 90f;
+    [Header("X Clamp")]
+    [SerializeField] private float minXClamp = -90f;
+    [SerializeField] private float maxXClamp = 90f;
+
+    private float yRotation = 0f;
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, minYClamp, maxYClamp);
+
+        xRotation += mouseX;
+        xRotation = Mathf.Clamp(xRotation, minXClamp, maxXClamp);
+        
+        transform.localRotation = Quaternion.Euler(yRotation, xRotation, 0f);
+    }
+
+}
