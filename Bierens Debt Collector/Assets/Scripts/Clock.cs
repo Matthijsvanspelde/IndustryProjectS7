354ab8e91d80ee57d621f06344c7e4fcdc481c01
Niels <389435@student.fontys.nl>
2021-12-08 10:17:21 +0100

    Finished up Vindhet.nl
    Added the final things to finish the Vindhet.nl page of the webbrowser application like ad's status bars and a working clock. Also created prefabs of the Interwebs page layout and the vindhet.nl layout.

---
+++ Bierens Debt Collector/Assets/Scripts/Clock.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+public class Clock : MonoBehaviour
+{
+    public Text textClock;
+
+    private DateTime time;
+    private string hour;
+    private string minute;
+
+    void Update()
+    {
+        time = DateTime.Now;
+        hour = LeadingZero(time.Hour);
+        minute = LeadingZero(time.Minute);
+        textClock.text = hour + ":" + minute;
+    }
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
