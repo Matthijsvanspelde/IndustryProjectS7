04bb4ed77347632df78c5c8f91f93d4afaa554aa
LeslieCrielaard <lesliecrielaard@gmail.com>
2021-12-09 15:53:48 +0100

    Interactable rendertexture

---
+++ Bierens Debt Collector/Assets/Scripts/RenderTextureInteraction/RenderTextureTest/ClickTestScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickTestScript : MonoBehaviour
+{
+public void ButtonClickedOnTexture()
+    {
+        Debug.Log("Texture has been clicked");
+    }
+}
