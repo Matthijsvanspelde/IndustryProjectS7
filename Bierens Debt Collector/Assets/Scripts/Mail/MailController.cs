6bdfa9245899b6bf0750d05c27c2cc7a5b5b4f9d
Gino <ginovandegraaf@hotmail.com>
2021-12-16 15:56:31 +0100

    Bierens Mail

---
+++ Bierens Debt Collector/Assets/Scripts/Mail/MailController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MailController : MonoBehaviour
+{
+    [SerializeField] private MailView mailView;
+    //we need a list of the story.
+    void CreateStoryMail()
+    {
+        MailModel mail = new MailModel(".", ".");
+        mailView.CreateNewMail(mail);
+    }
+
+  public void PayNowResponse()
+    {
+
+    }
+    public void ConfrontationResponse()
+    {
+
+    }
+
+    public void CallBierensResponse()
+    {
+
+    }
+
+
+}
---
+++ Bierens Debt Collector/Assets/Scripts/Mail/ResponseCode.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseCode 
+{
+    public enum SendResponse { PAYNOW, CONFRONTATION, CALLBIERENS, RESEARCHCONFRONTATION }
+    public SendResponse response;
+}
