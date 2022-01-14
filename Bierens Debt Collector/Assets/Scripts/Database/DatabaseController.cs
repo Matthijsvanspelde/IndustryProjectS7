b3e09314c7913996bed0a81c9e99897e17781a62
Jewels <j.klomp@student.fontys.nl>
2022-01-14 10:31:06 +0100

    Database.
    Zou moeten werken. Met  button. Moet nog getest worden.

--- Bierens Debt Collector/Assets/Scripts/Database/DatabaseController.cs
+++ Bierens Debt Collector/Assets/Scripts/Database/DatabaseController.cs
@@ -8,6 +8,8 @@ public class DatabaseController : MonoBehaviour
     public OrderController orderController;
     public GameObject orderDetailWindow;
 
+    public GameObject testRow;
+
     [SerializeField] private Transform content;
     [SerializeField] private GameObject prefabRow;
 
@@ -21,11 +23,11 @@ public class DatabaseController : MonoBehaviour
             //prefabRow.GetComponent<RowDisplay>().createRow();
             Instantiate(prefabRow, content);
         }
-        //openOrderDetails(rowList[1]);
+        openOrderDetails(testRow);
     }
 
-    public void openOrderDetails(Order order)
+    public void openOrderDetails(GameObject order)
     {
-        orderDetailWindow.GetComponent<OrderDetailDisplay>().showOrderDetails(orderDetailWindow, order);
+        orderDetailWindow.GetComponent<OrderDetailDisplay>().showOrderDetails(orderDetailWindow, order.GetComponent<RowDisplay>().orderScriptable);
     }
 }
